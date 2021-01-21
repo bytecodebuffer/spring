b52930387ed0940863ef182901ad7fcb2d1b373c
baizhuangzhuang <baizhuangzhuang>
2021-01-21 17:15:14 +0800

    LocalDate 工具类更新

---
+++ demo-tool-util/src/main/java/utils/date/LocalDateUtil.java
@@ -0,0 +1,26 @@
+package utils.date;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author bz
+ * @date 2021/1/21
+ */
+public class LocalDateUtil {
+
+    public static LocalDateTime str2datetime(String source) {
+        DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.parse(source,formatter);
+    }
+
+    public static String datetime2str(LocalDateTime source){
+        DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return formatter.format(source);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(str2datetime("2020-10-11 10:10:10"));
+        System.out.println(datetime2str(LocalDateTime.now()));
+    }
+}
